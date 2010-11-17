4564a2268c5f35d82470586dc7e98f486caefbfd
lluis <@192.168.1.100>
2010-11-17 12:50:40 +0100

    ff

--- SimpleAddin/uu.cs
+++ SimpleAddin/uu.cs
@@ -24,7 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
-namespace SimpleAddin // 12
+namespace SimpleAddin // 123
 {
 	public class uu
 	{
